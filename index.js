9ccf1ac81187c930b1b501c99d4c7c034d7321ae
burnick <arnelceledonio@gmail.com>
2016-01-07 16:59:27 +0800

    force refresh session

--- index.js
+++ index.js
@@ -367,9 +367,9 @@ module.exports = function (options) {
         });
       }
 
-      if (self.authLoginURL && (!req.session.refreshAfter || req.session.refreshAfter < Date.now())) {
+      //if (self.authLoginURL && (!req.session.refreshAfter || req.session.refreshAfter < Date.now())) {
         return refreshSession(req, res, next);
-      }
+      //}
 
       res.json({
         status: 'Valid Session',
