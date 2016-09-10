dff31c4c6a5d8166bc58a4e946f65579ff4f480e
Eugene Butusov <EvgenyButusov@gmail.com>
2016-09-10 23:13:16 +0300

    #176789053279444 Reset your password button is not working
    fix status code checking

--- index.js
+++ index.js
@@ -645,7 +645,7 @@ module.exports = function (options) {
       });
       hReq.on('error', next);
       hReq.on('response', function (resp) {
-        if (resp.statusCode !== 200 &&
+        if (resp.statusCode !== 204 &&
           resp.statusCode !== 400 &&
           resp.statusCode !== 401) {
           if (resp.statusCode === 429) {
