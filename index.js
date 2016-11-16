e27b07bd8bcc6e81fb72347355ebab51be677baa
Eugene Butusov <EvgenyButusov@gmail.com>
2016-11-16 13:27:01 +0300

    rollback setup changes

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ module.exports = function (options) {
       secret: self.secretKey,
       cookie: {
         expires: false,
-        secure: (self.forceSSL === true || self.forceSSL === 'true')
+        secure: self.forceSSL
       },
       proxy: true
     };
